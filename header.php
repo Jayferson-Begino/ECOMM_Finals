a131be9a00afb96a5fc57cfbe5301f84afefec0b
Jayferson-Begino <128022438+Jayferson-Begino@users.noreply.github.com>
2023-05-14 02:44:39 +0800

    product image fixed

--- header.php
+++ header.php
@@ -38,12 +38,12 @@ if(isset($message)){
          <div class="icons">
             <div id="menu-btn" class="fas fa-bars"></div>
             <a href="search_product.php" class="fas fa-search"></a>
-            <div id="user-btn" class="fas fa-user"></div>
             <?php
                $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
                $cart_rows_number = mysqli_num_rows($select_cart_number); 
             ?>
             <a href="cart.php"> <i class="fas fa-shopping-cart"></i> <span>(<?php echo $cart_rows_number; ?>)</span> </a>
+            <div id="user-btn" class="fas fa-user"></div>
          </div>
          
          <div class="user-box">
